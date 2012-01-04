fa9ead31816922b657ade93dc046d799c9de6b90
Tom O'Connor <tom.oconnor@baseblack.com>
2012-01-04 17:51:22 +0000

    fixing bugs

--- partitions.rb
+++ partitions.rb
@@ -107,9 +107,9 @@ if Facter.value(:kernel) == 'Linux'
   
   disks.each do |k,v|
     type = `/sbin/hdparm -I /dev/#{k}|/bin/grep "Nominal Media Rotation Rate" | /usr/bin/perl -n -e'/Nominal Media Rotation Rate:\s(.+)$/ && print $1'`
+    cyl_max = `/sbin/sfdisk -g /dev/#{k} |/usr/bin/perl -n -e'/(\d+) cylinders?/ && print $1'`
     if type.match(/Solid State/)
       ssdx << k
-      cyl_max = `/sbin/sfdisk -g /dev/#{k} |/usr/bin/perl -n -e'/(\d+) cylinders?/ && print $1'`
       ssds[k] = cyl_max
     end #if
   end #do
